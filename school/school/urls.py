6958ddda7458e8fafed63d8f0969c2da161f33af
Marioap <mariorabbap@gmail.com>
2015-09-12 19:01:00 +0900

    We succes finished comptetition

--- school/school/urls.py
+++ school/school/urls.py
@@ -3,5 +3,8 @@ from student import views
 
 urlpatterns = [
     url(r'^$', views.StudentList.as_view(), name='students'),
+    url(r'^create$', views.StudentCreate.as_view(), name='create'),
+    url(r'^delete/(?P<pk>\d+)/$', views.StudentDelete.as_view(), name='delete'),
+    url(r'^update/(?P<pk>\d+)/$', views.StudentUpdate.as_view(), name='update'),
     
 ]
--- school/student/views.py
+++ school/student/views.py
@@ -1,9 +1,27 @@
 from django.views.generic.list import ListView
-
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.core.urlresolvers import reverse_lazy
 from models import Student
    
 class StudentList(ListView):
     model = Student
+
+
+class StudentCreate(CreateView):
+    model = Student
+    fields = ['name', 'age', 'faculty']
+    success_url = reverse_lazy('students')
+
+
+class StudentDelete(DeleteView):
+    model = Student
+    success_url = reverse_lazy('students')
+
+
+class StudentUpdate(UpdateView):
+    model = Student
+    fields = ['name', 'age', 'faculty']
+    success_url = reverse_lazy('students')
 
     
     
