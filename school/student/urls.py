227306d4a300845f9aba55d114dbd36266080be7
John Doe <johndoe@example.com>
2015-09-15 11:59:35 +0900

    add base.html and move urls.py from school to student and urls.py from student to school

--- school/student/urls.py
+++ school/student/urls.py
@@ -1,23 +1,10 @@
-"""place URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.8/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Add an import:  from blog import urls as blog_urls
-    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
-"""
-from django.conf.urls import include, url
-from django.contrib import admin 
-from student.views import StudentList
+from django.conf.urls import url
+from student import views
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('students.urls')),
+    url(r'^$', views.StudentList.as_view(), name='students'),
+    url(r'^create$', views.StudentCreate.as_view(), name='create'),
+    url(r'^delete/(?P<pk>\d+)/$', views.StudentDelete.as_view(), name='delete'),
+    url(r'^update/(?P<pk>\d+)/$', views.StudentUpdate.as_view(), name='update'),
+    
 ]
