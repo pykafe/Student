56674bf0aa8e02d1fe6635147f5a1ea8b6eb93aa
Marioap <mariorabbap@gmail.com>
2015-09-16 16:26:22 +0900

    I added message for DeleteView

--- school/student/views.py
+++ school/student/views.py
@@ -2,9 +2,9 @@ from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.core.urlresolvers import reverse_lazy
 from django.contrib.messages.views import SuccessMessageMixin
-from django.contrib.messages import get_messages
+from django.contrib import messages
 from models import Student
-   
+
 class StudentList(ListView):
     model = Student
 
@@ -14,11 +14,16 @@ class StudentCreate(SuccessMessageMixin, CreateView):
     fields = ['name', 'age', 'faculty']
     success_url = reverse_lazy('students')
     success_message = "%(name)s was created successfully"
-    
+
 
 class StudentDelete(DeleteView):
     model = Student
     success_url = reverse_lazy('students')
+    success_message = "deleted successfully"
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super(StudentDelete, self).delete(request, *args, **kwargs)
 
 
 class StudentUpdate(SuccessMessageMixin, UpdateView):
